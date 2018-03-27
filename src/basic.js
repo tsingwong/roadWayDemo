c3d562c1d8c781cbab42be5a3d434e71e4225118
tsingwong <919916459@qq.com>
2018-03-27 20:59:25 +0800

    geometry

--- src/basic.js
+++ src/basic.js
@@ -2,7 +2,7 @@
  * @Author: tsingwong 
  * @Date: 2018-03-27 17:15:09 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2018-03-27 20:16:26
+ * @Last Modified time: 2018-03-27 20:56:44
  */
 let canvas = document.querySelector('#canvas');
 let stats;
@@ -21,7 +21,7 @@ function initRender() {
         // 开启抗锯齿
         antialias: true,
     });
-    renderer.setClearColor(0xffffff, 1.0);
+    renderer.setClearColor(0x000000, 1.0);
 }
 /**
  * 初始化相机
@@ -30,12 +30,12 @@ function initRender() {
 function initCamera() {
     //设置相机（视野，显示口的宽高比，近裁剪面，远裁剪面）
     camera = new THREE.PerspectiveCamera(
-        75,
+        45,
         canvas.width / canvas.height,
-        .1,
-        1000
+        1,
+        2000
     );
-    camera.position.set(0, 900, 0);
+    camera.position.y = 800;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 /**
@@ -50,8 +50,11 @@ function initScene() {
  * 
  */
 function initLight() {
-    light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
-    light.position.set(100, 100, 200);
+    // 环境光
+    scene.add(new THREE.AmbientLight(0x404040));
+    // 平衡光
+    light = new THREE.DirectionalLight(0xfffff, 1.0, 0);
+    light.position.set(0, 1, 0);
     scene.add(light);
 }
 /**
@@ -59,33 +62,83 @@ function initLight() {
  * 
  */
 function initModel() {
-    var geometry = new THREE.Geometry();
 
-    geometry.vertices.push(new THREE.Vector3(-500, 0, 0));
+    let map = new THREE.TextureLoader()
+        .load('../static/img/UV_Grid_Sm.jpg');
+    // 定义纹理在水平和垂直方向简单的重复到无穷大。
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    //定义纹理的各向异性
+    map.anisotropy = 16;
 
-    geometry.vertices.push(new THREE.Vector3(500, 0, 0));
+    // 兰伯特网孔材质
+    let material = new THREE.MeshLambertMaterial({ map: map, side: THREE.DoubleSide });
 
-    for (var i = 0; i <= 20; i++) {
+    //球形网格 （半径长度，水平块的密度，垂直块的密度）
+    let object = new THREE.Mesh(new THREE.SphereGeometry(75, 20, 10), material);
+    object.position.set(-400, 0, 200);
+    scene.add(object);
 
-        let line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: randomColor(), opacity: 1 }));
+    //二十面体 （图形大小半径，大于零将不是二十面体，越大越圆滑）
+    object = new THREE.Mesh(new THREE.IcosahedronGeometry(75, 0), material);
+    object.position.set(-200, 0, 200);
+    scene.add(object);
 
-        line.position.z = (i * 50) - 500;
+    //八面体（图形大小半径，大于零将不是八面体，越大越圆滑）
+    object = new THREE.Mesh(new THREE.OctahedronGeometry(75, 0), material);
+    object.position.set(0, 0, 200);
+    scene.add(object);
 
-        scene.add(line);
+    //四面体（图形大小半径，大于零将不是四面体，越大越圆滑）
+    object = new THREE.Mesh(new THREE.TetrahedronGeometry(75, 0), material);
+    object.position.set(200, 0, 200);
+    scene.add(object);
 
+    //长方形平面 （x轴宽度，y轴高度，x方向分段数，y方向分段数）
+    object = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 1, 1), material);
+    object.position.set(-400, 0, 0);
+    scene.add(object);
 
+    //立方体 （x轴宽度，y轴高度，z轴深度，沿宽面分段数，沿高度面分段数，沿深度面分段数）
+    object = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100, 1, 1, 1), material);
+    object.position.set(-200, 0, 0);
+    scene.add(object);
 
+    //圆形平面 （半径，顶点密度，绘制起点弧度，绘制弧度）
+    object = new THREE.Mesh(new THREE.CircleGeometry(50, 20, 0, Math.PI * 2), material);
+    object.position.set(0, 0, 0);
+    scene.add(object);
 
-        line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: randomColor(), opacity: 1 }));
+    //空心圆平面 （内圆半径，外圆半径，分割面越大越圆滑，垂直外边分割面，开始绘制弧度，绘制弧度）
+    object = new THREE.Mesh(new THREE.RingGeometry(25, 50, 10, 5, 0, Math.PI * 2), material);
+    object.position.set(200, 0, 0);
+    scene.add(object);
 
-        line.position.x = (i * 50) - 500;
+    //圆柱体 （头部圆的半径，底部圆半径，高度，上下圆顶点个数，上下面切割线条数，上下面是否显示，开始弧度，绘制弧度）
+    object = new THREE.Mesh(new THREE.CylinderGeometry(25, 75, 100, 40, 5), material);
+    object.position.set(400, 0, 0);
+    scene.add(object);
 
-        line.rotation.y = 90 * Math.PI / 180;
-
-        scene.add(line);
-
+    //车床模型
+    var points = [];
+    for (var i = 0; i < 50; i++) {
+        points.push(new THREE.Vector2(
+            Math.sin(i * 0.2) * Math.sin(i * 0.1) * 15 + 50, (i - 5) * 2));
     }
-    // console.log(scene);
+
+    //（一个vector2的数组分别代表xy轴，生成圆周段的数目，开始弧度，绘制弧度）
+    object = new THREE.Mesh(new THREE.LatheGeometry(points, 20), material);
+    object.position.set(-400, 0, -200);
+    scene.add(object);
+
+    //救生圈 （救生圈半径，管道直径，基于管道横切顶点数，救生圈横切顶点个数）
+    object = new THREE.Mesh(new THREE.TorusGeometry(50, 20, 20, 20), material);
+    object.position.set(-200, 0, -200);
+    scene.add(object);
+
+    //环面扭结模型 （图形半径，管道直径，基于管道横切定点数，根据图形半径横切顶点数，绕旋转对称轴的圈数，绕环面的圆的圈数）
+    object = new THREE.Mesh(new THREE.TorusKnotGeometry(50, 10, 50, 20), material);
+    object.position.set(0, 0, -200);
+    scene.add(object);
 }
 /**
  * 初始化辅助系统
@@ -99,6 +152,11 @@ function initAssist() {
     // // 辅助网格
     // gridHelper = new THREE.GridHelper(320, 32);
     // scene.add(gridHelper);
+
+    let object = new THREE.ArrowHelper(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 0), 100, 0x00ffff);
+    object.position.set(400, 0, -200);
+    scene.add(object);
+
 
     // 随意旋转拖动
     controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -121,24 +179,19 @@ function initAssist() {
  * 支持拖动
  * 
  */
-function animate () {
+function animate() {
     stats.begin();
-    //一秒钟调用60次，也就是以每秒60帧的频率来绘制场景。
-
     requestAnimationFrame(animate);
 
-
-
-    //console.log(cube.rotation);
-
-    //每次调用模型的沿xy轴旋转0.01
-
-    // cube.rotation.x += 0.01;
-
-    // cube.rotation.y += 0.01;  
-
-    //使用渲染器把场景和相机都渲染出来
-
+    let timer = Date.now() * 0.0001;
+    camera.position.x = Math.cos( timer ) * 800;
+    camera.position.z = Math.sin( timer ) * 800;
+    camera.lookAt( scene.position );
+    for ( var i = 0, l = scene.children.length; i < l; i ++ ) {
+        var object = scene.children[ i ];
+        object.rotation.x = timer * 5;
+        object.rotation.y = timer * 2.5;
+    }
     renderer.render(scene, camera);
     stats.end();
 
