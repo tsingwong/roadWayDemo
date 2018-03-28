e136f4c6f0ce3aadce63745b1791245d1c63b748
tsingwong <919916459@qq.com>
2018-03-28 15:08:19 +0800

    ConvexGeometry

--- src/basic.js
+++ src/basic.js
@@ -2,17 +2,17 @@
  * @Author: tsingwong 
  * @Date: 2018-03-27 17:15:09 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2018-03-28 13:33:03
+ * @Last Modified time: 2018-03-28 15:00:44
  */
 let canvas = document.querySelector('#canvas');
 let stats;
 
 /* global THREE, Stats, Detector, dat*/
-let renderer, camera, scene, ambientLight, pointLight, directionalLight;
+let renderer, camera, scene, ambientLight, pointLight, directionalLight, spotLight;
 
 let axisHelper, gridHelper, controls;
 
-let sphere, cube, plane;
+let sphere, cube, plane, line;
 
 let sphereMaterial, cubeMaterial;
 
@@ -73,23 +73,13 @@ function initLight() {
     ambientLight = new THREE.AmbientLight('#111111');
     scene.add(ambientLight);
     // 点光源
-    directionalLight = new THREE.DirectionalLight('#ffffff');
-    directionalLight.position.set(-40, 60, -10);
+    spotLight = new THREE.SpotLight('#ffffff');
+    spotLight.position.set(-40, 60, -10);
 
-
-    directionalLight.shadow.camera.near = 20; //产生阴影的最近距离
-    directionalLight.shadow.camera.far = 200; //产生阴影的最远距离
-    directionalLight.shadow.camera.left = -50; //产生阴影距离位置的最左边位置
-    directionalLight.shadow.camera.right = 50; //最右边
-    directionalLight.shadow.camera.top = 50; //最上边
-    directionalLight.shadow.camera.bottom = -50; //最下面
-    //这两个值决定使用多少像素生成阴影 默认512
-    directionalLight.shadow.mapSize.height = 1024;
-    directionalLight.shadow.mapSize.width = 1024;
 
     //开启阴影投射
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
 
     // let debug = new THREE.CameraHelper(directionalLight.shadow.camera);
 
@@ -100,32 +90,7 @@ function initLight() {
  * 
  */
 function initModel() {
-    let cubeGeometry = new THREE.CubeGeometry(25, 25, 25);
-    cubeMaterial = new THREE.MeshPhongMaterial({
-        color: '#eeeeee'
-    });
-    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(30, 5, -5);
-    cube.castShadow = true;
-    scene.add(cube);
-
-    let sphereGeometry = new THREE.SphereGeometry(10, 32, 32);
-    sphereMaterial = new THREE.MeshPhongMaterial({
-        color: 0x00ffff
-    });
-    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    sphere.position.set(-20, 20, 0);
-    sphere.castShadow = true;
-    scene.add(sphere);
-
-    let planeGeometry = new THREE.PlaneGeometry(5000, 5000, 20, 20);
-    let planeMaterial = new THREE.MeshPhongMaterial({color: 0xaaaaaa});
-    plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.y = -0;
-    plane.receiveShadow = true;
-    scene.add(plane);
-
+    generatePoints();
 }
 /**
  * 初始化辅助系统
@@ -152,7 +117,7 @@ function initAssist() {
     controls.dampingFactor = 0.25;
     // 旋转的速度
     controls.rotateSpeed = 0.35;
-    controls.autoRotate = false;
+    controls.autoRotate = true;
 
     stats = new Stats();
     stats.setMode(0);
@@ -222,4 +187,52 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     render();
     renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+//生成模型调用的方法
+
+function generatePoints() {
+    // 随机生成一组顶点
+    var points = [];
+    for (var i = 0; i < 20; i++) {
+        //xyz轴的坐标点的位置会随机生成在+-150以内
+        var randomX = -150 + Math.round(Math.random() * 300);
+        var randomY = -150 + Math.round(Math.random() * 300);
+        var randomZ = -150 + Math.round(Math.random() * 300);
+        //创建一个坐标点并添加到数组当中
+        points.push(new THREE.Vector3(randomX, randomY, randomZ));
+    }
+
+    //声明一个存放所有点的网格对象
+    let spGroup = new THREE.Object3D();
+    //声明一个网格基础材质
+    var material = new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: false });
+    //遍历数组生成小球点并添加到对象当中
+    points.forEach(function (point) {
+        var spGeom = new THREE.SphereGeometry(5);
+        var spMesh = new THREE.Mesh(spGeom, material);
+        spMesh.position.copy(point); //将当前小球的位置设置为当前点的坐标
+        scene.add(spMesh);
+    });
+    // 将存放所有点的对象添加到场景当中
+    scene.add(spGroup);
+    // 使用这些点实例化一个THREE.ConvexGeometry几何体对象
+    var hullGeometry = new THREE.ConvexGeometry(points);
+    //生成模型
+    let hullMesh = createMesh(hullGeometry);
+    //添加到场景
+    scene.add(hullMesh);
+}
+
+function createMesh(geom) {
+    // 实例化一个绿色的半透明的材质
+    var meshMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.2});
+    meshMaterial.side = THREE.DoubleSide; //将材质设置成正面反面都可见
+    var wireFrameMat = new THREE.MeshBasicMaterial();
+    wireFrameMat.wireframe = true; //把材质渲染成线框
+
+    // 将两种材质都赋给几何体
+    var mesh = THREE.SceneUtils.createMultiMaterialObject(geom, [meshMaterial, wireFrameMat]);
+    return mesh;
 }
